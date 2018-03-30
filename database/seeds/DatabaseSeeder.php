d8956c8515f6c8fe903671fda26304e81c3df888
Duda <andreyduda88@gmail.com>
2018-03-30 23:23:44 +0300

    dev

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert(
+            [//password 123456
+                [
+                    'name'     => 'admin',
+                    'email'    => 'admin@gmail.com',
+                    'password' => '$2y$10$UN/QeftQP5ONnnKoyfRxPui3hnyP5.8fIlgT1QBUsSjYWAHy0Uv0W'
+                ]
+            ]);
     }
 }
